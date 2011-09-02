da51e1bc768c1b294705fe8415042e702ceb53db
Mikhail Trishchenkov <kriomant@gmail.com>
2011-09-02 23:21:50 +0700

    Exit on connection error.

--- transmission-unity-launcher.py
+++ transmission-unity-launcher.py
@@ -10,7 +10,9 @@ References:
 	transmissionrpc documentation: http://packages.python.org/transmissionrpc/
 	https://blueprints.launchpad.net/ubuntu/+spec/desktop-o-default-apps-unity-integration
 """
+import sys
 import logging
+
 from gi.repository import Unity, Gio, GObject, Dbusmenu
 import transmissionrpc
 
@@ -25,34 +27,59 @@ LAUNCHER_ENTRY_NAME = 'transmission-gtk.desktop'
 
 UPDATE_INTERVAL = 20 # seconds
 
+def is_connection_error(error):
+	http_error_class = transmissionrpc.httphandler.HTTPHandlerError
+	return isinstance(error.original, http_error_class) and error.original.code == 111
+
 # Connect to Transmission.
 logging.info("Try to connect to Transmision at %s:%d as %s.",
 	TRANSMISSION_RPC_HOST, TRANSMISSION_RPC_PORT, TRANSMISSION_RPC_USER
 )
-transmission = transmissionrpc.Client(
-	address=TRANSMISSION_RPC_HOST,
-	port=TRANSMISSION_RPC_PORT,
-	user=TRANSMISSION_RPC_USER,
-	password=TRANSMISSION_RPC_PASSWORD,
-)
+try:
+	transmission = transmissionrpc.Client(
+		address=TRANSMISSION_RPC_HOST,
+		port=TRANSMISSION_RPC_PORT,
+		user=TRANSMISSION_RPC_USER,
+		password=TRANSMISSION_RPC_PASSWORD,
+	)
+except transmissionrpc.transmission.TransmissionError as error:
+	logging.exception("Failed to connect")
+	if is_connection_error(error):
+		sys.stderr.write("""Unable to connect to Transmission at %s:%d.
+Ensure it is running and web interface is enabled at this address.
+""" % (TRANSMISSION_RPC_HOST, TRANSMISSION_RPC_PORT))
+		sys.exit(1)
+	else:
+		raise
 
 logging.debug("Get launcher entry %s", LAUNCHER_ENTRY_NAME)
 launcher = Unity.LauncherEntry.get_for_desktop_id(LAUNCHER_ENTRY_NAME)
 
-def update_status():
-	# Get list of torrents.
-	logging.debug("Get torrents list.")
-	torrents = transmission.list()
+def update_status(quit):
+	try:
+		# Get list of torrents.
+		logging.debug("Get torrents list.")
+		torrents = transmission.list()
 
-	# Filter only downloading ones.
-	downloading_torrent_ids = [t.id for t in torrents.values() if t.status == 'downloading']
+		# Filter only downloading ones.
+		downloading_torrent_ids = [t.id for t in torrents.values() if t.status == 'downloading']
 
-	logging.debug("%d of %d are downloading", len(downloading_torrent_ids), len(torrents))
+		logging.debug("%d of %d are downloading", len(downloading_torrent_ids), len(torrents))
 
-	# Get detailed information about downloading torrents.
-	# 'id' fields is required by transmissionrpc to sort results and 'name' field
-	# is used by Torrent.__repr__.
-	infos = transmission.info(downloading_torrent_ids, ['id', 'name', 'sizeWhenDone', 'leftUntilDone'])
+		# Get detailed information about downloading torrents.
+		# 'id' fields is required by transmissionrpc to sort results and 'name' field
+		# is used by Torrent.__repr__.
+		infos = transmission.info(downloading_torrent_ids, ['id', 'name', 'sizeWhenDone', 'leftUntilDone'])
+	except transmissionrpc.transmission.TransmissionError as error:
+		logging.exception("Failed to connect")
+		if is_connection_error(error):
+			sys.stderr.write("""Connection to Transmission is lost.
+Quit.
+""")
+			quit() # Terminate application loop.
+			return False # Stop timer.
+		else:
+			raise
 
 	# Calculate total torrents size and downloaded amount.
 	total_size = left_size = 0
@@ -72,9 +99,11 @@ def update_status():
 	launcher.set_property('progress', progress)
 	launcher.set_property('progress_visible', torrents_count > 0)
 
+	return True # Leave timer active.
+
 loop = GObject.MainLoop()
 
-update_status()
-GObject.timeout_add_seconds(UPDATE_INTERVAL, update_status)
+update_status(loop.quit)
+GObject.timeout_add_seconds(UPDATE_INTERVAL, update_status, loop.quit)
 
 loop.run()
