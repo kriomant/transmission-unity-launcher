7aa5bcb9eb0dafc3afa80a924d52a1c85d8b036f
Mikhail Trishchenkov <kriomant@gmail.com>
2011-09-04 15:01:17 +0700

    Accept command-line arguments.
    Default settings may be overriden by command-line arguments.
    Currently these are connection parameters and update interval.

--- transmission-unity-launcher.py
+++ transmission-unity-launcher.py
@@ -12,20 +12,45 @@ References:
 """
 import sys
 import logging
+import argparse
 
 from gi.repository import Unity, Gio, GObject, Dbusmenu
 import transmissionrpc
 
+parser = argparse.ArgumentParser(description="Integrate Transmission into Unity Launcher.")
+parser.add_argument('-l', '--launcher-entry-name',
+	action='store', dest='launcher_entry_name',
+	default='transmission-gtk.desktop',
+	help="name of .desktop file (including extension) used to start Transmission"
+)
+parser.add_argument('-H', '--transmission-host',
+	action='store', dest='transmission_host',
+	default='localhost',
+	help="address for connecting to Transmission RPC"
+)
+parser.add_argument('-p', '--transmission-port',
+	action='store', dest='transmission_port', type=int,
+	default=9091,
+	help="port for connecting to Transmission RPC"
+)
+parser.add_argument('-U', '--transmission-user',
+	action='store', dest='transmission_user',
+	default=None,
+	help="user name for connecting to Transmission RPC"
+)
+parser.add_argument('-P', '--transmission-password',
+	action='store', dest='transmission_password',
+	default=None,
+	help="password for connecting to Transmission RPC"
+)
+parser.add_argument('-u', '--update-interval',
+	action='store', dest='update_interval', type=int,
+	default=20,
+	help="interval (in seconds) between status updates"
+)
+args = parser.parse_args()
+
 logging.basicConfig(level=logging.DEBUG)
-
-TRANSMISSION_RPC_HOST = 'localhost'
-TRANSMISSION_RPC_PORT = 9091
-TRANSMISSION_RPC_USER = None
-TRANSMISSION_RPC_PASSWORD = None
-
-LAUNCHER_ENTRY_NAME = 'transmission-gtk.desktop'
-
-UPDATE_INTERVAL = 20 # seconds
 
 def is_connection_error(error):
 	http_error_class = transmissionrpc.httphandler.HTTPHandlerError
@@ -33,27 +58,27 @@ def is_connection_error(error):
 
 # Connect to Transmission.
 logging.info("Try to connect to Transmision at %s:%d as %s.",
-	TRANSMISSION_RPC_HOST, TRANSMISSION_RPC_PORT, TRANSMISSION_RPC_USER
+	args.transmission_host, args.transmission_port, args.transmission_user
 )
 try:
 	transmission = transmissionrpc.Client(
-		address=TRANSMISSION_RPC_HOST,
-		port=TRANSMISSION_RPC_PORT,
-		user=TRANSMISSION_RPC_USER,
-		password=TRANSMISSION_RPC_PASSWORD,
+		address=args.transmission_host,
+		port=args.transmission_port,
+		user=args.transmission_user,
+		password=args.transmission_password,
 	)
 except transmissionrpc.transmission.TransmissionError as error:
 	logging.exception("Failed to connect")
 	if is_connection_error(error):
 		sys.stderr.write("""Unable to connect to Transmission at %s:%d.
 Ensure it is running and web interface is enabled at this address.
-""" % (TRANSMISSION_RPC_HOST, TRANSMISSION_RPC_PORT))
+""" % (args.transmission_host, args.transmission_port))
 		sys.exit(1)
 	else:
 		raise
 
-logging.debug("Get launcher entry %s", LAUNCHER_ENTRY_NAME)
-launcher = Unity.LauncherEntry.get_for_desktop_id(LAUNCHER_ENTRY_NAME)
+logging.debug("Get launcher entry %s", args.launcher_entry_name)
+launcher = Unity.LauncherEntry.get_for_desktop_id(args.launcher_entry_name)
 
 def update_status(quit):
 	try:
@@ -104,6 +129,7 @@ Quit.
 loop = GObject.MainLoop()
 
 update_status(loop.quit)
-GObject.timeout_add_seconds(UPDATE_INTERVAL, update_status, loop.quit)
+GObject.timeout_add_seconds(args.update_interval, update_status, loop.quit)
 
 loop.run()
+
