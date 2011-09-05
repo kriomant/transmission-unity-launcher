0347edd5e660c1c77025b6ba5c0a674ec8a8cc8d
Mikhail Trishchenkov <kriomant@gmail.com>
2011-09-05 21:50:58 +0700

    Start Transmission and wait for it's exit.
    Start Transmission when started. Command to start Transmission is
    taken from command line. Then wait until Transmission is closed
    and exit.
    Wait a little after starting Transmission and before trying to
    connect to give time for initialization. Delay is 2 seconds by
    default and may be changed using --startup-timeout option.

--- transmission-unity-launcher.py
+++ transmission-unity-launcher.py
@@ -14,7 +14,7 @@ import sys
 import logging
 import argparse
 
-from gi.repository import Unity, Gio, GObject, Dbusmenu
+from gi.repository import Unity, Gio, GLib, GObject, Dbusmenu
 import transmissionrpc
 
 parser = argparse.ArgumentParser(description="Integrate Transmission into Unity Launcher.")
@@ -43,68 +43,72 @@ parser.add_argument('-P', '--transmission-password',
 	default=None,
 	help="password for connecting to Transmission RPC"
 )
+parser.add_argument('-t', '--startup-timeout',
+	default=4,
+	help="time (in seconds) between Transmission start and first connection attempt"
+)
 parser.add_argument('-u', '--update-interval',
 	action='store', dest='update_interval', type=int,
 	default=20,
 	help="interval (in seconds) between status updates"
 )
+parser.add_argument('transmission_command',
+	nargs='+',
+	help="command and arguments to start Transmission"
+)
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.DEBUG)
+
+loop = GObject.MainLoop()
+
+# Start transmission.
+flags = (
+	# Inherit PATH environment variable.
+	GLib.SpawnFlags.SEARCH_PATH |
+
+	# Don't reap transmission process automatically so it is possible
+	# to detect when Transmission is closed.
+	GLib.SpawnFlags.DO_NOT_REAP_CHILD
+)
+_, transmission_pid = GLib.spawn_async(
+	None, # Inherit current directory,
+	args.transmission_command, # Command with arguments.
+	None, # Inherit environment.
+	flags,
+	None, # Child setup callback.
+	None  # User data.
+)
+logging.info("Transmission started (pid: %d).", transmission_pid)
+
+# Exit when Transmission is closed.
+def transmission_closed(pid, status, data):
+	logging.info("Transmission exited with status %d, exiting.", status)
+	GLib.spawn_close_pid(pid)
+	loop.quit()
+GLib.child_watch_add(GLib.PRIORITY_DEFAULT, transmission_pid, transmission_closed, None)
 
 def is_connection_error(error):
 	http_error_class = transmissionrpc.httphandler.HTTPHandlerError
 	return isinstance(error.original, http_error_class) and error.original.code == 111
 
-# Connect to Transmission.
-logging.info("Try to connect to Transmision at %s:%d as %s.",
-	args.transmission_host, args.transmission_port, args.transmission_user
-)
-try:
-	transmission = transmissionrpc.Client(
-		address=args.transmission_host,
-		port=args.transmission_port,
-		user=args.transmission_user,
-		password=args.transmission_password,
-	)
-except transmissionrpc.transmission.TransmissionError as error:
-	logging.exception("Failed to connect")
-	if is_connection_error(error):
-		sys.stderr.write("""Unable to connect to Transmission at %s:%d.
-Ensure it is running and web interface is enabled at this address.
-""" % (args.transmission_host, args.transmission_port))
-		sys.exit(1)
-	else:
-		raise
-
 logging.debug("Get launcher entry %s", args.launcher_entry_name)
 launcher = Unity.LauncherEntry.get_for_desktop_id(args.launcher_entry_name)
 
-def update_status(quit):
-	try:
-		# Get list of torrents.
-		logging.debug("Get torrents list.")
-		torrents = transmission.list()
+def update_status(transmission):
+	# Get list of torrents.
+	logging.debug("Get torrents list.")
+	torrents = transmission.list()
 
-		# Filter only downloading ones.
-		downloading_torrent_ids = [t.id for t in torrents.values() if t.status == 'downloading']
+	# Filter only downloading ones.
+	downloading_torrent_ids = [t.id for t in torrents.values() if t.status == 'downloading']
 
-		logging.debug("%d of %d are downloading", len(downloading_torrent_ids), len(torrents))
+	logging.debug("%d of %d are downloading", len(downloading_torrent_ids), len(torrents))
 
-		# Get detailed information about downloading torrents.
-		# 'id' fields is required by transmissionrpc to sort results and 'name' field
-		# is used by Torrent.__repr__.
-		infos = transmission.info(downloading_torrent_ids, ['id', 'name', 'sizeWhenDone', 'leftUntilDone'])
-	except transmissionrpc.transmission.TransmissionError as error:
-		logging.exception("Failed to connect")
-		if is_connection_error(error):
-			sys.stderr.write("""Connection to Transmission is lost.
-Quit.
-""")
-			quit() # Terminate application loop.
-			return False # Stop timer.
-		else:
-			raise
+	# Get detailed information about downloading torrents.
+	# 'id' fields is required by transmissionrpc to sort results and 'name' field
+	# is used by Torrent.__repr__.
+	infos = transmission.info(downloading_torrent_ids, ['id', 'name', 'sizeWhenDone', 'leftUntilDone'])
 
 	# Calculate total torrents size and downloaded amount.
 	total_size = left_size = 0
@@ -124,12 +128,65 @@ Quit.
 	launcher.set_property('progress', progress)
 	launcher.set_property('progress_visible', torrents_count > 0)
 
+def first_update():
+	try:
+		# Connect to Transmission.
+		logging.info("Try to connect to Transmision at %s:%d as %s.",
+			args.transmission_host, args.transmission_port, args.transmission_user
+		)
+		try:
+			transmission = transmissionrpc.Client(
+				address=args.transmission_host,
+				port=args.transmission_port,
+				user=args.transmission_user,
+				password=args.transmission_password,
+			)
+		except transmissionrpc.transmission.TransmissionError as error:
+			logging.exception("Failed to connect")
+			if is_connection_error(error):
+				sys.stderr.write("""Unable to connect to Transmission at %s:%d.
+		Ensure it is running and web interface is enabled at this address.
+		""" % (args.transmission_host, args.transmission_port))
+				loop.quit()
+			else:
+				raise
+
+		# Try to update status for the first time.
+		update_status(transmission)
+
+		# If all is ok, start main timer.
+		GObject.timeout_add_seconds(args.update_interval, periodic_update, transmission)
+
+	except transmissionrpc.transmission.TransmissionError as error:
+		loop.quit() # Terminate application loop.
+		if is_connection_error(error):
+			sys.stderr.write("""Can't connect to Transmission.
+Quit.
+""")
+		else:
+			raise
+
+	finally:
+		return False # Stop timer.
+
+def periodic_update(transmission):
+	try:
+		update_status(transmission)
+	except transmissionrpc.transmission.TransmissionError as error:
+		if is_connection_error(error):
+			logging.error("Connection to Transmission is lost.")
+			sys.stderr.write("""Connection to Transmission is lost.
+Quit.
+""")
+			loop.quit() # Terminate application loop.
+			return False # Stop timer.
+		else:
+			logging.exception("Failed to connect")
+			raise
+
 	return True # Leave timer active.
 
-loop = GObject.MainLoop()
-
-update_status(loop.quit)
-GObject.timeout_add_seconds(args.update_interval, update_status, loop.quit)
+GObject.timeout_add_seconds(args.startup_timeout, first_update)
 
 loop.run()
 
